f5cf54602f6fc640ffd36c7409c67873dcdd6384
wangyinhui <13037651897@163.com>
2020-12-20 10:08:52 +0800

    zk_api

---
+++ src/main/java/com/my/zk/zk_api/conf/myConf.java
@@ -0,0 +1,13 @@
+package com.my.zk.zk_api.conf;
+
+import lombok.Data;
+
+/**
+ * @author wangyh
+ * @create 2020-12-20 9:37
+ */
+@Data
+public class myConf {
+	private String ip;
+	private Integer port;
+}
---
+++ src/test/java/com/my/zk/zk_api/ZkApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.my.zk.zk_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class ZkApiApplicationTests {
+
+		@Test void contextLoads( ) {
+		}
+
+}
